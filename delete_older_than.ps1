bcb2bb83c73221b5e51f1f0aeedeb4dff729af58
Jure Forstneric <jforst@gmail.com>
2019-08-29 15:56:33 +0200

    Created script to delete files older than x days

---
+++ delete_older_than.ps1
@@ -0,0 +1,18 @@
+﻿#For hard-coding a path into our script.
+
+# $path = "C:\path\to\folder\"
+
+#For passing the path as an argument when running the script.
+#Example: delete_older_than.ps1 -path "C:\path\to\folder\"
+
+#param([string]$path)
+
+#For passing multiple paths from a text file and passing the text file as an argument. 
+#Second argument is age of files (defaults to 90).
+
+param([string]$list, [Int32]$days=90)
+
+$paths = Get-Content -Path $list
+
+Get-ChildItem -Path $paths | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-$days) } | Remove-Item
+
